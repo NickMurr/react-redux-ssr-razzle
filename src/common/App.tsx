6ce56ea0dcac551d58b0289ba8c36eac832536b3
nickmurr <32221055+NickMurr@users.noreply.github.com>
2019-08-28 15:56:38 +0300

    test

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import './App.scss';
+import logo from './react.svg';
 import About from './components/About';
 
 const App = () => (
-  <Switch>
-    <Route exact={true} path="/" component={Home} />
-    <Route exact={true} path="/about" component={About} />
-  </Switch>
+  <div className="Home">
+    <div className="Home-header">
+      <img src={logo} className="Home-logo" alt="logo" />
+      <h2>Welcome to Razzles</h2>
+    </div>
+
+    <Switch>
+      <Route exact={true} path="/" component={Home} />
+      <Route exact={true} path="/about" component={About} />
+    </Switch>
+  </div>
 );
 
 export default App;
--- src/common/components/About.tsx
+++ src/common/components/About.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../react.svg';
 
 const About = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <h1>This is About Page</h1>
+      <div className="Home">
+        <div className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h2>Welcome to Razzles</h2>
+        </div>
+        <h1>About</h1>
+        <ul className="Home-resources">
+          <Link to="/">Home</Link>
+        </ul>
+      </div>
     </div>
   );
 };
